9b5eb63c684f7eb0b1b8263c3d1150cb2da3a2bc
Jason Cross <jason@bunkerventures.com>
2024-04-19 15:21:57 +0930

    debug stop script

--- scripts/stop_docker.sh
+++ scripts/stop_docker.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# Get the full path to the script
+SCRIPT_PATH="$(realpath $0)"
+
+# Print the script path
+echo "This script is located at: $SCRIPT_PATH"
+
+# Optionally, to just get the directory and not the script filename:
+SCRIPT_DIR="$(dirname $SCRIPT_PATH)"
+echo "This script's directory is: $SCRIPT_DIR"
+
+# Name of the file you want to display, located in the same directory as the script
+FILE_NAME="stop_docker.sh"
+
+# Full path to the file
+FULL_PATH="$SCRIPT_DIR/$FILE_NAME"
+
+# Display the content of the file
+cat "$FULL_PATH"
+
 # Fetch all running container IDs
 CONTAINER_IDS=$(docker ps -aq)
 
