38320cc9a7bb6491485173f34640cbf9dde79686
Jason Cross <jason@bunkerventures.com>
2024-04-19 14:53:33 +0930

    testing deploy

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
-# Start Docker containers
-docker start $(docker ps -aq)
-# Or using Docker Compose
-# docker-compose -f /path/to/your/docker-compose.yml up -d
 
+# Path to the imagedefinitions.json file
+JSON_FILE="imagedefinitions.json"
+
+# Extract the Docker image URI using jq
+IMAGE_URI=$(jq -r '.[0].imageUri' $JSON_FILE)
+
+if [[ -z "$IMAGE_URI" ]]; then
+  echo "Error: Unable to find image URI in $JSON_FILE"
+  exit 1
+fi
+
+echo "Pulling Docker image: $IMAGE_URI"
+
+# Pull the latest Docker image
+docker pull $IMAGE_URI
+
+if [[ $? -ne 0 ]]; then
+  echo "Error: Failed to pull Docker image"
+  exit 1
+fi
+
+echo "Starting new Docker container with image: $IMAGE_URI"
+
+# Start a new Docker container
+docker run -d --name my-container \
+  -p 80:80 \
+  $IMAGE_URI
+
+if [[ $? -eq 0 ]]; then
+  echo "Successfully started new Docker container."
+else
+  echo "Error: Failed to start new Docker container"
+  exit 1
+fi
