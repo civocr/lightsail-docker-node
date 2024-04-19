642040c816d803154e0a90759552838fc9eb8c05
Jason Cross <jason@bunkerventures.com>
2024-04-19 10:45:01 +0930

    debug prebuild

---
+++ scripts/prebuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Print environment variables
+env
+
+# Locate docker executable
+which docker
+
+# Print Docker version
+docker --version
+
+# Log in to Amazon ECR
+echo "Logging in to Amazon ECR..."
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 533267141943.dkr.ecr.us-east-1.amazonaws.com
