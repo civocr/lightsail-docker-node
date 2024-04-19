dec1564e89cb1586d285fbbbd059d5ef8b422b4e
Jason Cross <jason@bunkerventures.com>
2024-04-19 15:01:49 +0930

    clean up docker file

--- scripts/stop_docker.sh
+++ scripts/stop_docker.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-# Stop all Docker containers
-docker stop $(docker ps -aq)
 
+# Fetch all running container IDs
+CONTAINER_IDS=$(docker ps -aq)
+
+# Check if any containers are running
+if [ -z "$CONTAINER_IDS" ]; then
+  echo "No containers are currently running."
+else
+  echo "Stopping the following containers: $CONTAINER_IDS"
+  docker stop $CONTAINER_IDS
+  if [ $? -eq 0 ]; then
+    echo "Containers stopped successfully."
+  else
+    echo "Failed to stop some or all containers."
+  fi
+fi
