940f1759a645924be9a667e66a2f3459a4b3851e
Jason Cross <jason@bunkerventures.com>
2024-04-24 15:02:16 +0930

    testing

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Example of a simple HTTP check
-if curl -s http://localhost:3000/ | grep "Expected output" > /dev/null
+if curl -s http://localhost:3000/ | grep "Hello World! I am here" > /dev/null
 then
   echo "Service is running."
   exit 0
