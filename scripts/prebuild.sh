6ab962a3173d1c71c0f8051b2019b2cb9e238ca1
Jason Cross <jason@bunkerventures.com>
2024-04-24 15:07:33 +0930

    fix syntax

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -11,5 +11,5 @@ docker --version
 
 # Log in to Amazon ECR
 echo "Logging in to Amazon ECR..."
-echo "ECR: ${$ECR_REPOSITORY_URI}"
+echo "ECR: ${ECR_REPOSITORY_URI}"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 533267141943.dkr.ecr.us-east-1.amazonaws.com
